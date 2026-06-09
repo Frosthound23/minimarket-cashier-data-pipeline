f907fe694cb4591d6fc8e0ec39c5fb3795ee997e
frosthound23 <thrallthe1@gmail.com>
2026-06-09 16:46:55 +0700

    clean up code

--- dbt/minimarket_dbt/models/marts/fact_sales.sql
+++ dbt/minimarket_dbt/models/marts/fact_sales.sql
@@ -16,19 +16,19 @@ final as (
 
     select
         ti.item_id as sales_id,
-        t.transaction_id,
-        ti.item_id,
+        t.transaction_id as transaction_id,
+        ti.item_id as item_id,
         t.customer_id as customer_key,
         ti.product_id as product_key,
         toYYYYMMDD(toDate(t.transaction_date)) as date_key,
-        t.store_id,
-        t.transaction_date,
-        t.payment_method,
-        ti.quantity,
-        ti.unit_price,
-        ti.discount,
-        ti.subtotal,
-        t.loaded_at
+        t.store_id as store_id,
+        t.transaction_date as transaction_date,
+        t.payment_method as payment_method,
+        ti.quantity as quantity,
+        ti.unit_price as unit_price,
+        ti.discount as discount,
+        ti.subtotal as subtotal,
+        t.loaded_at as loaded_at
     from transaction_items ti
     inner join transactions t
         on ti.transaction_id = t.transaction_id
