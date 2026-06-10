77f2ee9c803e6db7e679c792d416c914e7facddf
frosthound23 <thrallthe1@gmail.com>
2026-06-10 22:19:46 +0700

    fix alias fact

--- dbt/minimarket_dbt/models/marts/fact_promotion_usage.sql
+++ dbt/minimarket_dbt/models/marts/fact_promotion_usage.sql
@@ -3,31 +3,31 @@
 select
     concat(tp.tenant_id, '-', toString(tp.transaction_promotion_id)) as promotion_usage_key,
 
-    tp.tenant_id,
+    tp.tenant_id as tenant_id,
 
     concat(tp.tenant_id, '-', toString(tp.transaction_id)) as transaction_key,
     concat(tp.tenant_id, '-', toString(tp.promo_id)) as promotion_key,
 
-    tp.transaction_promotion_id,
-    tp.transaction_id,
-    tp.promo_id,
+    tp.transaction_promotion_id as transaction_promotion_id,
+    tp.transaction_id as transaction_id,
+    tp.promo_id as promo_id,
 
-    t.customer_id,
-    t.store_id,
+    t.customer_id as customer_id,
+    t.store_id as store_id,
 
     concat(t.tenant_id, '-', toString(t.customer_id)) as customer_key,
     concat(t.tenant_id, '-', toString(t.store_id)) as store_key,
 
-    t.transaction_date,
-    t.transaction_date_key,
+    t.transaction_date as transaction_date,
+    t.transaction_date_key as transaction_date_key,
 
-    p.promo_name,
-    p.promo_type,
-    p.discount_pct,
-    p.min_purchase,
+    p.promo_name as promo_name,
+    p.promo_type as promo_type,
+    p.discount_pct as discount_pct,
+    p.min_purchase as min_purchase,
 
-    tp.discount_applied,
-    t.total_amount,
+    tp.discount_applied as discount_applied,
+    t.total_amount as total_amount,
 
     t.loaded_at as transaction_loaded_at,
     tp.loaded_at as promotion_usage_loaded_at
--- dbt/minimarket_dbt/models/marts/fact_sales.sql
+++ dbt/minimarket_dbt/models/marts/fact_sales.sql
@@ -3,31 +3,31 @@
 select
     concat(ti.tenant_id, '-', toString(ti.item_id)) as sales_key,
 
-    ti.tenant_id,
+    ti.tenant_id as tenant_id,
 
     concat(t.tenant_id, '-', toString(t.transaction_id)) as transaction_key,
     concat(t.tenant_id, '-', toString(t.customer_id)) as customer_key,
     concat(t.tenant_id, '-', toString(t.store_id)) as store_key,
     concat(ti.tenant_id, '-', toString(ti.product_id)) as product_key,
 
-    t.transaction_id,
-    ti.item_id,
-    t.customer_id,
-    t.store_id,
-    ti.product_id,
+    t.transaction_id as transaction_id,
+    ti.item_id as item_id,
+    t.customer_id as customer_id,
+    t.store_id as store_id,
+    ti.product_id as product_id,
 
-    t.transaction_date,
-    t.transaction_date_key,
+    t.transaction_date as transaction_date,
+    t.transaction_date_key as transaction_date_key,
 
-    t.payment_method,
-    t.status,
+    t.payment_method as payment_method,
+    t.status as status,
 
-    ti.quantity,
-    ti.unit_price,
-    ti.discount,
-    ti.subtotal,
+    ti.quantity as quantity,
+    ti.unit_price as unit_price,
+    ti.discount as discount,
+    ti.subtotal as subtotal,
 
-    t.total_amount,
+    t.total_amount as total_amount,
 
     t.loaded_at as transaction_loaded_at,
     ti.loaded_at as item_loaded_at
