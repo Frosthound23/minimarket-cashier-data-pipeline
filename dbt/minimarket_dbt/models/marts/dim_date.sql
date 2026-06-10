88381fd5a0d44bbab049913a99a01ed40def348e
frosthound23 <thrallthe1@gmail.com>
2026-06-10 19:39:40 +0700

    update dim_date

--- dbt/minimarket_dbt/models/marts/dim_date.sql
+++ dbt/minimarket_dbt/models/marts/dim_date.sql
@@ -1,31 +1,20 @@
-with date_range as (
+{{ config(materialized='table') }}
 
+with date_spine as (
     select distinct
-        toDate(transaction_date) as date_day
+        transaction_date_key as date_day
     from {{ ref('stg_transactions') }}
-
-),
-
-date_enriched as (
-
-    select
-        date_day,
-        toDayOfWeek(date_day) as day_of_week
-    from date_range
-
 )
 
 select
-    toYYYYMMDD(date_day) as date_key,
     date_day,
     toYear(date_day) as year,
-    toQuarter(date_day) as quarter,
     toMonth(date_day) as month,
-    formatDateTime(date_day, '%M') as month_name,
-    toDayOfMonth(date_day) as day_of_month,
-    day_of_week,
+    toDayOfMonth(date_day) as day,
+    toQuarter(date_day) as quarter,
+    toDayOfWeek(date_day) as day_of_week,
 
-    case day_of_week
+    case toDayOfWeek(date_day)
         when 1 then 'Monday'
         when 2 then 'Tuesday'
         when 3 then 'Wednesday'
@@ -33,6 +22,25 @@ select
         when 5 then 'Friday'
         when 6 then 'Saturday'
         when 7 then 'Sunday'
-    end as day_name
+    end as day_name,
 
-from date_enriched
+    case toMonth(date_day)
+        when 1 then 'January'
+        when 2 then 'February'
+        when 3 then 'March'
+        when 4 then 'April'
+        when 5 then 'May'
+        when 6 then 'June'
+        when 7 then 'July'
+        when 8 then 'August'
+        when 9 then 'September'
+        when 10 then 'October'
+        when 11 then 'November'
+        when 12 then 'December'
+    end as month_name,
+
+    case
+        when toDayOfWeek(date_day) in (6, 7) then true
+        else false
+    end as is_weekend
+from date_spine
